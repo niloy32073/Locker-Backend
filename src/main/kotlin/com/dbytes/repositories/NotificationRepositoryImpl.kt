7e1acc745593b0f2c2779be9b3ad3d782ecb64fc
niloy32073 <mdnrahmanx27@gmail.com>
2025-03-06 22:26:03 +0600

    100% done

--- src/main/kotlin/com/dbytes/repositories/NotificationRepositoryImpl.kt
+++ src/main/kotlin/com/dbytes/repositories/NotificationRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.dbytes.models.Notification
 import com.dbytes.tables.NotificationTable
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class NotificationRepositoryImpl:NotificationRepository {
     override suspend fun addNotification(notification: Notification) {
@@ -15,21 +16,29 @@ class NotificationRepositoryImpl:NotificationRepository {
 
     override suspend fun getNotifications(id: Long): List<Notification> {
         if(id != 0L){
-            val notifications = NotificationTable.selectAll().where { NotificationTable.userId eq id }.map { Notification(
-                id = it[NotificationTable.id],
-                message = it[NotificationTable.message],
-                timestamp = it[NotificationTable.timestamp],
-                userId = it[NotificationTable.userId]
-            ) }
+            val notifications =  transaction {
+                NotificationTable.selectAll().where { NotificationTable.userId eq id }.map {
+                    Notification(
+                        id = it[NotificationTable.id],
+                        message = it[NotificationTable.message],
+                        timestamp = it[NotificationTable.timestamp],
+                        userId = it[NotificationTable.userId]
+                    )
+                }
+            }
             return notifications
         }
         else{
-            val notifications = NotificationTable.selectAll().map { Notification(
-                id = it[NotificationTable.id],
-                message = it[NotificationTable.message],
-                timestamp = it[NotificationTable.timestamp],
-                userId = it[NotificationTable.userId]
-            ) }
+            val notifications = transaction {
+                NotificationTable.selectAll().map {
+                    Notification(
+                        id = it[NotificationTable.id],
+                        message = it[NotificationTable.message],
+                        timestamp = it[NotificationTable.timestamp],
+                        userId = it[NotificationTable.userId]
+                    )
+                }
+            }
             return notifications
         }
     }
